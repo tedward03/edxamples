c2dd935d2425f6ca6df84dd0fd36d18dc89a1516
tedward03 <tedward603@gmail.com>
2018-10-05 17:50:22 +0100

    Initial commit for react examples project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MrEd from './resources/mrEd.png';
 import './App.css';
+import bojack from './resources/bojack.jpg'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+        <nav className="navbar navbar-expand-sm bg-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="navbar-brand" to="/"><img className="navbarbrandImage" src={bojack}/></Link>
+            </li>
+            <li class="nav-item">
+              <Link className="nav-link" to="/examples">Examples</Link>
+            </li>
+            <li class="nav-item">
+              <Link className="nav-link" to="/betterExamples">BetterExamples</Link>
+            </li>
+          </ul>
+        </nav>
+         <Route exact path="/" component= {Landing}/>
+         <Route path="/examples" component= {Examples}/>    
+         <Route path="/betterExamples" component= {BetterExamples}/>    
+
+      </div>    
     );
   }
+}
+
+export const Landing = () => {
+  return (
+    <header className="App-header">
+          <img src={MrEd}/>
+          <p>
+            What kind of name is Ed for a developer?
+          </p>
+    </header>
+  );
+} 
+
+export const Examples = () => {
+  return ( 
+    <ol> 
+      <li>
+        Routing
+      </li>
+    </ol>
+  );
+}
+
+export const BetterExamples = () => {
+  return ( 
+    <ol> 
+      <li>
+        Better Routing
+      </li>
+    </ol>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Landing, Examples} from './App' // todo change this and where they are coming from
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  )
+  // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
